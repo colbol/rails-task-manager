f1447b7b051d3bdf4986d3331b44b3caff1ae6ba
Matthew Collis <matthew.collis@mail.mcgill.ca>
2017-07-04 14:31:56 -0400

    first update1

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704135109) do
+ActiveRecord::Schema.define(version: 20170704182449) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.string "string"
+    t.string "name"
     t.string "details"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
